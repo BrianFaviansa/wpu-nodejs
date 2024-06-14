fc21701208927fbb3bb6e306f7cf6ee25afe813e
BrianFaviansa <brianfpd31@gmail.com>
2024-06-15 06:30:19 +0700

    9 menggunakan package npm done

--- 8. npm/app.js
+++ 8. npm/app.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('hibou@gmail.com')); 
+// console.log(validator.isMobilePhone('082244433306', 'id-ID')); 
+// console.log(validator.isNumeric('082244433306')); 
+
+// console.log(chalk.italic.bgBlue('Hello world!'));
+const nama = 'Hibou';
+const pesan = chalk`Lorem ipsum dolor {bgRed.black.bold sit amet} consectetur, {bgGreen.italic.black adipisicing} elit. Dicta, nesciunt?, Nama saya : ${nama}`;
+console.log(pesan);
+
