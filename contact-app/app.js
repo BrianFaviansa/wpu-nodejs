db067d64aee5a7426d97974e6520253ca41a8a7e
BrianFaviansa <brianfpd31@gmail.com>
2024-06-15 08:38:50 +0700

    12 contact app bagian 3 done

--- contact-app/app.js
+++ contact-app/app.js
@@ -1,5 +1,5 @@
 const yargs = require('yargs');
-const { simpanContact } = require('./contacts');
+const { simpanContact, listContact, detailContact, deleteContact } = require('./contacts');
 // * mengambil argument dari command line
 
 yargs.command({
@@ -25,29 +25,50 @@ yargs.command({
   handler(argv) {
     simpanContact(argv.nama, argv.email, argv.noHP);
   }
-});
+}).demandCommand();
+
+// * menampilkan daftar semua contact yang berisi nama dan no hp
+yargs.command({
+  command: 'list',
+  describe: 'Menampilkan semua nama dan no HP',
+  handler() {
+    listContact()
+  }
+})
+
+// * menampilkan detail sebuah contact
+yargs.command({
+  command: 'detail',
+  describe: 'Menampilkan detail sebuah contact berdasarkan nama',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    detailContact(argv.nama)
+  }
+})
+
+// * menghapus contact berdasarkan nama
+yargs.command({
+  command: 'delete',
+  describe: 'menghapus contact berdasarkan nama',
+  builder: {
+    nama: {
+      describe: 'Nama lengkap',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    deleteContact(argv.nama)
+  }
+})
 
 yargs.parse();
 
 
-
-
-
-
-
-
-
-
-
-// const { tulisPertanyaan, simpanContact } = require('./contacts');
-
-// const main = async () => {
-//   const nama = await tulisPertanyaan('Masukkan nama anda : ');
-//   const email = await tulisPertanyaan('Masukkan email anda : ');
-//   const noHP = await tulisPertanyaan('Masukkan no hp anda : ');
-
-//   simpanContact(nama, email, noHP);
-// }
-
-// main();
 
