3a4850615c1c5a8e207d630ce7d0b76caab96652
BrianFaviansa <brianfpd31@gmail.com>
2024-06-15 11:12:18 +0700

    15 express view engine done

--- 12 express view engine/app.js
+++ 12 express view engine/app.js
@@ -1,27 +1,54 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
+// * gunakan ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
 app.get("/", (req, res) => {
-  // res.send("Hello World!");
-  // res.json({
-  //   nama: 'Hibou',
-  //   email: 'hibou@gmail.com',
-  //   noHP: '082244433508'
-  // });
-  res.sendFile("./index.html", { root: __dirname });
+  // res.sendFile("./index.html", { root: __dirname });
+  const mahasiswa = [
+    {
+      nama: "Hibou",
+      email: "hibou@gmail.com",
+    },
+    {
+      nama: "Bebou",
+      email: "bebou@gmail.com",
+    },
+    {
+      nama: "Furby",
+      email: "furby@gmail.com",
+    },
+  ];
+  res.render("index", {
+    title: "Halaman Home",
+    layout: "layouts/main-layout",
+    nama: "Hibou Programming",
+    mahasiswa,
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./about.html", { root: __dirname });
+  res.render("about", {
+    title: "Halaman About",
+    layout: "layouts/main-layout",
+  });
 });
 
-app.get('/product/:id', (req, res) => {
-  res.send(`Product ID: ${req.params.id} <br> Category : ${req.query.category}`)
-})
-
 app.get("/contact", (req, res) => {
-  res.sendFile("./contact.html", { root: __dirname });
+  res.render("contact", {
+    title: "Halaman Contact",
+    layout: "layouts/main-layout",
+  });
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(
+    `Product ID: ${req.params.id} <br> Category : ${req.query.category}`
+  );
 });
 
 app.use("/", (req, res) => {
